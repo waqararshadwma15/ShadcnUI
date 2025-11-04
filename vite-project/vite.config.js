ada93068585bb0e88529e828e0a7d12629d926e4
Waqar Arshad <waqararshadwma15@gmail.com>
2025-11-04 16:20:01 +0500

    Second

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,6 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_URL || "/vite-project",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
