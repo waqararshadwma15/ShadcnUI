5c2a65066fd489437f92a69ac2d7d50dfd17e9b9
Waqar Arshad <waqararshadwma15@gmail.com>
2025-11-04 16:04:42 +0500

    ShadCN Installation

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+import Header from "./components/Header";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Button variant='outline'>Hello World</Button>{" "}
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-amber-200'>Header</div>
+  )
+}
+
+export default Header
